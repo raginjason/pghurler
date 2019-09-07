f53931e108597162a14d4c8cf3ec683286eabe68
Jason Walker <ragin.jason@me.com>
2019-09-07 10:10:37 -0400

    Generated using cobra

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2019 Jason Walker <ragin.jason@me.com>
+This file is part of pghurler.
+*/
+package main
+
+import "github.com/raginjason/pghurler/cmd"
+
+func main() {
+	cmd.Execute()
+}
